2eea6116fa3d6ad3b50d189b1be159de579dc8d2
barteksielicki <barteksielicki@gmail.com>
2017-11-18 19:44:15 +0100

    tide settings and add celery broker url

--- jobsbrowser/jobsbrowser/settings.py
+++ jobsbrowser/jobsbrowser/settings.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # Scrapy settings for jobsbrowser project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     http://doc.scrapy.org/en/latest/topics/settings.html
-#     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = 'jobsbrowser'
 
@@ -15,75 +8,54 @@ SPIDER_MODULES = ['jobsbrowser.spiders']
 NEWSPIDER_MODULE = 'jobsbrowser.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# SER_AGENT = 'jobsbrowser (+http://www.yourdomain.com)'
+# USER_AGENT = 'jobsbrowser (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-# ONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html# ownload-delay
 # See also autothrottle settings and docs
-# OWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-# ONCURRENT_REQUESTS_PER_DOMAIN = 16
-# ONCURRENT_REQUESTS_PER_IP = 16
-
-# Disable cookies (enabled by default)
-# OOKIES_ENABLED = False
-
-# Disable Telnet Console (enabled by default)
-# ELNETCONSOLE_ENABLED = False
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
 # Override the default request headers:
-# EFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
 # }
 
-# Enable or disable spider middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-# PIDER_MIDDLEWARES = {
-#    'jobsbrowser.middlewares.JobsbrowserSpiderMiddleware': 543,
-# }
-
-# Enable or disable downloader middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-# OWNLOADER_MIDDLEWARES = {
-#    'jobsbrowser.middlewares.MyCustomDownloaderMiddleware': 543,
-# }
-
-# Enable or disable extensions
-# See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
-# XTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-# }
-
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-# TEM_PIPELINES = {
-#    'jobsbrowser.pipelines.JobsbrowserPipeline': 300,
-# }
+ITEM_PIPELINES = {
+    'jobsbrowser.pipelines.JobsbrowserPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-# UTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-# UTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-# UTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-# UTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-# UTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html# ttpcache-middleware-settings
-# TTPCACHE_ENABLED = True
-# TTPCACHE_EXPIRATION_SECS = 0
-# TTPCACHE_DIR = 'httpcache'
-# TTPCACHE_IGNORE_HTTP_CODES = []
-# TTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# Celery Broker
+CELERY_BROKER = "pyamqp://guest@localhost//"
